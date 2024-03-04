df4da0d4f17b24f5347164875a202c84b06eeccd
mahnooshr <mahnoosh_mr79@yahoo.com>
2024-03-04 07:20:56 +0330

    hyghybgh

--- scanner.py
+++ scanner.py
@@ -8,36 +8,6 @@ Type_of_tokens = {
 "WHITESPACE"  : "\n\r\t\v\f"
 }
 
-def is_num(str):
-    if str in Type_of_tokens["NUM"]:
-        return True
-    else:
-        return False
-
-def is_id(str):
-    if str in Type_of_tokens["ID"]:
-        if str[0] in Type_of_tokens["letters"]:
-            return True
-    else:
-        return False
-
-def is_keyword(str):
-    if str in Type_of_tokens["KEYWORD"]:
-        return True
-    else:
-        return False
-
-def is_symbol(str):
-    if str in Type_of_tokens["SYMBOL"]:
-        return True
-    else:
-        return False
-
-
-
-
-l_num=1
-
 tokens=[]
 
 class Token:   
@@ -48,25 +18,52 @@ class Token:
         self.type=type
         tokens.append(self)
         
-    def get_token(self):
-        recognize_token = str((self.type , self.value)) 
-        return recognize_token
-
-
-def search_in_line (line):
-    current_loc=0
-    words=line.split(" ")
-    for word in words:
-        if is_keyword(word):
-            word=Token(word,"KEYWORD")
-        elif is_symbol(word):
-            word=Token(word,"SYMBOL")
-        elif is_num(word):
-            word=Token(word,"NUM")
-        elif is_id(word):
-            word=Token(word,"ID")   
+    def __str__ (self):
+        return f"({self.type}, {self.value})"
+    
+    
+    
+    
+class Line:
+    
+    def __init(self,number,pointer_loc,str):    
+        self.line_number=number
+        self.pointer_loc=pointer_loc
+        self.str=str
+        
+    def __Str__(self):
+        return self.pointer_loc
+        
+def lookahead_NUM (j,str):
+    token_str=""
+    line=Line(i,line)
+    for i in range(line.pointer_loc,len(str)):
+        if str[i] in Type_of_tokens["NUM"] :
+            token_str+=str[i]
+            line.pointer_loc+=1
         else:
-                 
+            break
+    return Token(token_str,"NUM")
+  
+def get_next_token (i,line):
+    n=len(line)
+    l_num=i
+    line=Line(i,line)
+    line.pointer_loc=0
+    
+    if line[line.pointer_loc] in Type_of_tokens ["SYMBOL"]:
+        Token(line[line.pointer_loc],"SYMBOL")
+        
+    else:
+        token_str=""
+        while line[line.pointer_loc] in Type_of_tokens["NUM"]
+            token_str+=line[line.pointer_loc]
+            line.pointer_loc+=1
+            lookahead_NUM(l_num,line)
+            
+    elif line[line.pointer_loc]in Type_of_tokens["letter"] :
+        lookahead_NUM(line)    
+  
             
     
 
@@ -76,6 +73,7 @@ file2=open("tokens.txt", "w")
 line_list = file1.readlines();
 for line in line_list:
     str = line
+    get_next_token(line_list.index(line),line)
     ;
     ;
     ;
