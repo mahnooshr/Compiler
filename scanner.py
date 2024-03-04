6bcfbd357225e194efba8a918abb6a1d81023847
mahnooshr <mahnoosh_mr79@yahoo.com>
2024-03-04 06:04:34 +0330

    nbhuj

--- scanner.py
+++ scanner.py
@@ -1,32 +1,52 @@
 Type_of_tokens = {
  "NUM" : "0123456789",
- "ID" : "[a-zA-Z]*",
+ "ID" : "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+ "letters" : "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
 "KEYWORD": ["if", "else", "void", "int", "for", "break", "return", "endif"],
-"SYMBOL" :  "; : , [ ] ( ) { } + - * = < ==",
+"SYMBOL" : [";", ":" ,"," ,"[", "]", "(", ")", "{", "}", "+", "-", "*", "=", "<", "=="],
 "COMMENT " :"///*",
 "WHITESPACE"  : "\n\r\t\v\f"
 }
 
 def is_num(str):
+    if str in Type_of_tokens["NUM"]:
+        return True
+    else:
+        return False
 
 def is_id(str):
+    if str in Type_of_tokens["ID"]:
+        if str[0] in Type_of_tokens["letters"]:
+            return True
+    else:
+        return False
 
 def is_keyword(str):
+    if str in Type_of_tokens["KEYWORD"]:
+        return True
+    else:
+        return False
 
 def is_symbol(str):
+    if str in Type_of_tokens["SYMBOL"]:
+        return True
+    else:
+        return False
 
 
 
 
 l_num=1
 
+tokens=[]
 
 class Token:   
-    tokens=[]
+    
         
     def __init__(self , value , type):
         self.value=value
         self.type=type
+        tokens.append(self)
         
     def get_token(self):
         recognize_token = str((self.type , self.value)) 
@@ -38,11 +58,21 @@ def search_in_line (line):
     words=line.split(" ")
     for word in words:
         if is_keyword(word):
-            word=Token(word,"keyword")
+            word=Token(word,"KEYWORD")
+        elif is_symbol(word):
+            word=Token(word,"SYMBOL")
+        elif is_num(word):
+            word=Token(word,"NUM")
+        elif is_id(word):
+            word=Token(word,"ID")   
+        else:
+                 
+            
     
 
      
 file1 = open("input.txt", "r") 
+file2=open("tokens.txt", "w")
 line_list = file1.readlines();
 for line in line_list:
     str = line
@@ -54,7 +84,7 @@ for line in line_list:
 file1.close() 
 
 
-file2=open("tokens.txt", "w")
+
 file2.write(l_num +   )
 
 
